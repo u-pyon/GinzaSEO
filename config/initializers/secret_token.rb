3c31aef427c73d43e840d7359e029790cb5d3d7a
Yuichi Oikawa <yuichi.oikawa@gmail.com>
2012-12-27 00:20:01 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GinzaSEO::Application.config.secret_token = '99c38a006167a9efefdc8d95680c6f4e9b3a2d2c831c19c0624748b8712981c80bd3d44602bef795a08589b8e2e06cc3d4391fd6efb9c10fc199847e50fd5041'
